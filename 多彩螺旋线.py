7add21fca6fc72804851ea6e72f4f3e75a453161
李小辉 <70791792+li594@users.noreply.github.com>
2020-09-12 22:57:17 -0500

    Add files via upload

---
+++ 多彩螺旋线.py
@@ -0,0 +1,15 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> t = turtle.Pen()
+>>> def draw_spin():
+	colores = ['orange','blue','yellow','red','purple','green']
+	turtle.bgcolor('black')
+	for x in range(300):		
+		turtle.pencolor (colores[x % 6])
+		turtle.width(x/100 + 1)
+		turtle.forward (x)
+		turtle.right(59)
+
+		
+>>> draw_spin()
